d172ff590962f0d0eff608e438a9c31380261611
AdAbd <adezzed@gmail.com>
2023-11-23 09:55:51 +0100

    Exercice 7 CORRECTION

---
+++ exo7.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p "Veuillez entrer un nom de fichier : " nom_fichier
+
+read -p "Voulez-vous créer le fichier $nom_fichier ? (oui/non) : " confirmation
+
+if [ "$confirmation" == "oui" ]; then
+    # Créer le fichier
+    touch "$nom_fichier"
+    echo "Le fichier $nom_fichier a été créé."
+else
+  
+    echo "Opération annulée. Aucun fichier n'a été créé."
+fi
